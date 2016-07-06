f21afa0631c3391ceaa79546b2fdb185ee5155f5
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-07-06 16:21:02 +0200

    feat(reducer): add reducer

---
+++ js/reducers/reducer.js
@@ -0,0 +1,51 @@
+define([
+  'constants',
+  'lib/utils'
+], ({ ACTION_TYPES: at }, { removeFromArrayByID }) => {
+
+  return getInitialState => (state = getInitialState(), action = {}) => {
+    switch (action.type) {
+      case at.UPDATE_MEDIA_LIST:
+        return {
+          ...state,
+          //list: removeFromArrayByID(state.list, action.item.id).concat([action.item]),
+          list: action.list,
+        };
+
+      case at.FILTER:
+        return {
+          ...state,
+          filter: action.filter,
+        };
+
+      case at.SORT:
+        return {
+          ...state,
+          sortBy: action.sortBy,
+          sortDirection: action.sortDirection,
+        };
+
+      case at.ADD_TO_WATCHLIST:
+        const filteredWatchlist = removeFromArrayByID(state.watchlist || [], action.id);
+        return {
+          ...state,
+          watchlist: filteredWatchlist.concat([{ id: action.id, date: action.date }]),
+        };
+
+      case at.REMOVE_FROM_WATCHLIST:
+        return {
+          ...state,
+          watchlist: removeFromArrayByID(state.watchlist || [], action.id),
+        };
+
+      case at.SET_POLL_INTERVAL:
+        return {
+          ...state,
+          interval: action.interval,
+        };
+
+      default:
+        return state;
+    }
+  };
+});
