38047aed413b86bc0bcdae25139e20dd86f66880
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-07-06 16:25:07 +0200

    feat(Sorter): add Sorter component

---
+++ js/components/Sorter.js
@@ -0,0 +1,58 @@
+define([
+  'constants',
+  'lib/virtual-dom',
+  'stores/store',
+  'actions/media-list',
+  'lib/utils'
+], ({ SORTABLE_PROPERTIES }, VirtualDom, Store, { sortMediaList }, { capitalize }) => {
+
+  class Sorter extends VirtualDom.Component {
+    componentDidUpdate() {
+      const $this = $(this.$dom);
+      $this.on('change', () => {
+        const sortBy = $this.find('#sort-property').val();
+        const sortDirection = $this.find('[name="sort-direction"]:checked').val();
+        Store.dispatch(sortMediaList(sortBy, sortDirection));
+      });
+    }
+
+    shouldComponentUpdate() { return false; }
+
+    render() {
+      const { sortBy, sortDirection } = this.props;
+      const options = SORTABLE_PROPERTIES.map(item => (
+        <option value={item} selected={sortBy === item}>
+          {item.length <=2 ? item.toUpperCase() : capitalize(item)}
+        </option>
+      ));
+      return (
+        <form>
+          <h2>Sort</h2>
+          <select name="sort-property" id="sort-property">
+            {options}
+          </select>
+          <label>
+            <input
+              type="radio"
+              name="sort-direction"
+              value="asc"
+              checked={sortDirection === 'asc'}
+            />
+            Asc
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort-direction"
+              value="desc"
+              checked={sortDirection === 'desc'}
+            />
+            Desc
+          </label>
+        </form>
+      );
+    }
+  }
+
+  return Sorter;
+});
