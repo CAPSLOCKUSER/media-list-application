113aef6033ab85586a7d9cfbacfd12e1ed570980
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-07-06 16:28:42 +0200

    feat(index.js): update index.js with the new App component

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 require(['lib/virtual-dom', 'components/App'], (VirtualDom, App) => {
-  VirtualDom.register(document.body, <App />);
+  const app = <App url="http://146.185.158.18/fake_api.php" />;
+  VirtualDom.register(document.body, app);
 });
