2562edf8ad76c1275dff1e0653e6e7f0db72c286
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-07-06 16:28:14 +0200

    feat(App): build up the App component

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,24 @@
-define(['lib/virtual-dom'], (VirtualDom) => {
+define([
+  'lib/virtual-dom',
+  'components/MediaHolder',
+  'components/Filter',
+  'components/Sorter',
+  'stores/store',
+], (VirtualDom, MediaHolder, Filter, Sorter, Store) => {
 
   class App extends VirtualDom.Component {
+
+    shouldComponentUpdate() { return true; }
+
     render() {
+      const { filter, sortBy, sortDirection } = Store.getState();
       return (
-        <h1>Media list application</h1>
+        <div class="container">
+          <h1>Media list application</h1>
+          <Sorter sortBy={sortBy} sortDirection={sortDirection} />
+          <Filter filter={filter} />
+          <MediaHolder url={this.props.url} />
+        </div>
       );
     }
   }
