7c157fa3c0addc8533fca6b71ef17fec6f02b119
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-07-12 13:15:01 +0200

    feat(jQuery): add jquery as private AMD dependency

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+require.config({
+  paths: {
+    'jquery': '../vendor/jquery-3.0.0.min',
+  },
+  map: {
+    '*': { 'jquery': 'jquery-private' },
+    'jquery-private': { 'jquery': 'jquery' }
+  },
+});
+
 require(['lib/virtual-dom', 'components/App'], (VirtualDom, App) => {
   const app = <App url="http://146.185.158.18/fake_api.php" />;
   VirtualDom.register(document.body, app);
